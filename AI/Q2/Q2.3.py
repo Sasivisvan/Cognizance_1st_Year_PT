656e51dda5722407f4dead6a9417209a541c344f
Sasivisvan <80504986+Sasivisvan@users.noreply.github.com>
2024-09-10 00:14:30 +0530

    Create Q2.3.py

---
+++ AI/Q2/Q2.3.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+data = [
+    [170, 65, 19, 85, 5],
+    [180, 75, 20, 90, 6],
+    [160, 55, 18, 80, 4],
+    [175, 70, 21, 88, 7],
+    [155, 50, 19, 82, 5],
+    [165, 62, 22, 89, 6],
+    [178, 80, 23, 91, 7],
+    [162, 58, 20, 78, 3],
+    [172, 68, 19, 86, 5],
+    [169, 66, 20, 84, 4],
+    [171, 64, 22, 87, 6],
+    [177, 72, 21, 90, 9],
+    [174, 76, 24, 88, 8],
+    [158, 52, 18, 75, 3],
+    [164, 63, 19, 81, 4]
+]
+# column names being ‘Height’, ‘Weight’, ‘Age’, ‘Avg_Grade’ and ‘Courses’ in that order.
+
+df = pd.DataFrame(data, columns=['Height', 'Weight','Age', 'Avg_Grade' ,'Courses'])
+
+print(df.value_counts('Age'))
